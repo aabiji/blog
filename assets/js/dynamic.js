b7667a856d204bb2be7cfe1826b99e891639254e
Abigail <adegbijiabigail@gmail.com>
2022-12-27 18:16:19 -0600

    Blog engine: create new blog

---
+++ assets/js/dynamic.js
@@ -0,0 +1,5 @@
+
+let date = new Date().getFullYear();
+let msg_element = document.getElementById("msg");
+let msg = `<a href='https://github.com/aabiji'>@aabiji</a> 2022 - ${date}`;
+msg_element.innerHTML = msg;
---
+++ engine/example.md
@@ -0,0 +1,4 @@
+# Hello world from markdown!
+
+I'll be writing my blog articles using the custom markdown 
+flavor I built.
